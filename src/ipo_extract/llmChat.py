f9c386693df4d4370ea9771731f9a1fac7bd265a
zhou <zjiongfeng@tianjianpro.com>
2025-04-06 23:23:11 +0800

    所有表格识别

--- src/ipo_extract/llmChat.py
+++ src/ipo_extract/llmChat.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-prompt1 = """
+prompt11 = """
 #### 定位
 - 智能助手名称 ：数据采集员
 - 主要任务 ：对任务材料中的上市公司年度财报的内容进行分析，判断该内容片段是否披露《重要在建工程项目本期变动情况》的表格。
@@ -13,9 +13,10 @@ prompt1 = """
   - 能够准确分析《重要在建工程项目本期变动情况》下的表格，是否披露相应表格，分析是否包含表头和数据，都包含则视为披露，如果只有表头没有任何数据则视为未披露。
   - 能够准确分析《重要在建工程项目本期变动情况》的章节下，如没有表格则需要判断是否标注不适用，未披露情况下一般会标注不适用。披露情况下一般会不标注或标注适用。
   - 能够分析出自己是否能对这次任务做出准确判断，如数据披露过少，表头部分确实等无法做出准确判断则该认为需要人工介入
+  - 注意是《重要在建工程项目本期变动情况》，不是《在建工程情况》
 
 #### 知识储备
-- 财务知识 ：《重要在建工程项目本期变动情况》一般包含以下内容，部分情况下也可能没有
+- 财务知识 ：《重要在建工程项目本期变动情况》一般包含以下大部分内容
   - 项目名称
   - 预算数
   - 期初余额
@@ -31,6 +32,33 @@ prompt1 = """
 #### 任务材料
 """
 
+prompt12 = """
+#### 定位
+- 智能助手名称 ：数据采集员
+- 主要任务 ：对任务材料中的上市公司年度财报的内容进行分析，判断该内容片段是否披露《重要在建工程项目本期变动情况》的表格。
+
+#### 能力
+- 文本分析 ：
+  - 能够准确识别《合同资产情况》的章节，不受其他章节干扰。
+  - 能够准确识别《合同资产情况》的章节下，是否存在相应表格。
+  - 能够准确分辨《合同资产情况》下的表格是否有被拆分成多个表格的情况，能结合被拆分的表格统一进行分析。
+  - 能够准确分析《合同资产情况》下的表格，是否披露相应表格，分析是否包含表头和数据，都包含则视为披露，如果只有表头没有任何数据则视为未披露。
+  - 能够准确分析《合同资产情况》的章节下，如没有表格则需要判断是否标注不适用，未披露情况下一般会标注不适用。披露情况下一般会不标注或标注适用。
+  - 能够分析出自己是否能对这次任务做出准确判断，如数据披露过少，表头部分确实等无法做出准确判断则该认为需要人工介入
+
+#### 知识储备
+- 财务知识 ：《合同资产情况》一般包含以下大部分内容
+  - 项目
+  - 期末余额
+  - 账面余额
+  - 坏账准备
+  - 账面价值
+
+#### 任务材料
+"""
+
+prompt1s = [prompt11, prompt12]
+
 prompt2 = """
 #### 输出说明
 - 输出 ：最后根据思考过程得出结论。结论只输出“是”来表示披露或“否”来表示不披露。如无法判断则输出“需要人工介入”，不需要额外解释。
@@ -38,50 +66,46 @@ prompt2 = """
 
 client = OpenAI(
     # 如果没有配置环境变量，请用百炼API Key替换：api_key="sk-xxx"
-    api_key="sk-622c6d00c975492786eba2e6a2a93ec4",
+    api_key="sk-22",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
-def chat_completion(message):
+
+def chat_completion(message, i=0):
     # 初始化OpenAI客户端
-    try:
-        reasoning_content = ""  # 定义完整思考过程
-        answer_content = ""  # 定义完整回复
-        last = ""
-        is_answering = False  # 判断是否结束思考过程并开始回复
-        # 创建聊天完成请求
-        completion = client.chat.completions.create(
-            model="qwq-32b",  # 此处以 qwq-32b 为例，可按需更换模型名称
-            messages=[
-                {"role": "user", "content": prompt1 + message + prompt2}
-            ],
-            # QwQ 模型仅支持流式输出方式调用
-            stream=True,
-            # 解除以下注释会在最后一个chunk返回Token使用量
-            stream_options={
-                "include_usage": True
-            }
-        )
-        print("\n" + "=" * 20 + "思考过程" + "=" * 20 + "\n")
-        for chunk in completion:
-            # 如果chunk.choices为空，则打印usage
-            if not chunk.choices:
-                last = chunk.usage
+    reasoning_content = ""  # 定义完整思考过程
+    answer_content = ""  # 定义完整回复
+    last = None  # 定义完整回复
+    is_answering = False  # 判断是否结束思考过程并开始回复
+    # 创建聊天完成请求
+    completion = client.chat.completions.create(
+        model="qwq-32b",  # 此处以 qwq-32b 为例，可按需更换模型名称
+        messages=[
+            {"role": "user", "content": prompt1s[i] + message + prompt2}
+        ],
+        # QwQ 模型仅支持流式输出方式调用
+        stream=True,
+        # 解除以下注释会在最后一个chunk返回Token使用量
+        stream_options={
+            "include_usage": True
+        }
+    )
+    for chunk in completion:
+        # 如果chunk.choices为空，则打印usage
+        if not chunk.choices:
+            last = chunk.usage
+        else:
+            delta = chunk.choices[0].delta
+            # 打印思考过程
+            if hasattr(delta, 'reasoning_content') and delta.reasoning_content is not None:
+                reasoning_content += delta.reasoning_content
             else:
-                delta = chunk.choices[0].delta
-                # 打印思考过程
-                if hasattr(delta, 'reasoning_content') and delta.reasoning_content != None:
-                    reasoning_content += delta.reasoning_content
-                else:
-                    # 开始回复
-                    if delta.content != "" and is_answering is False:
-                        print("\n" + "=" * 20 + "完整回复" + "=" * 20 + "\n")
-                        is_answering = True
-                    # 打印回复过程
-                    answer_content += delta.content
-    except Exception as error:
-        print(error)
-    return reasoning_content, answer_content,last
+                # 开始回复
+                if delta.content != "" and is_answering is False:
+                    is_answering = True
+                # 打印回复过程
+                answer_content += delta.content
+    return reasoning_content, answer_content, last
 
 #
 # if __name__ == '__main__':
--- src/ipo_extract/table_publish.py
+++ src/ipo_extract/table_publish.py
@@ -18,12 +18,37 @@ chinese_num_map = {
 
 # 正则表达式模式
 main_pattern = re.compile(
-    r'^\(?(一|二|三|四|五|六|七|八|九|十|十一|十二|十三|十四|十五|十六)、?\)?\s*(重要会计政策及会计估计|税项|会计政策和会计估计变更以及前期差错更正的说明|(合并)?财务报表(主要)?项目(注释|附注)|研发支出|合并范围的(变更|变动)|在其他主体中的权益)$')
-target_pattern = re.compile(r'^(合并)?财务报表(主要)?项目(注释|附注)$')
+    r'^\(?(一|二|三|四|五|六|七|八|九|十|十一|十二|十三|十四|十五|十六)、?\)?\s*(重要会计政策及会计估计|税项|会计政策和会计估计变更以及前期差错更正的说明|(合并)?财务报表(主要)?项目(注释|附注)|研发支出|合并范围的(变更|变动)|在其他主体中的权益)|政府补助|与金融工具相关的风险|公允价值的披露|关联方及关联交易$')
+target_pattern1 = re.compile(r'^(合并)?财务报表(主要)?项目(注释|附注)$')
+target_pattern2 = re.compile(r'^合并范围的(变更|变动)$')
+target_pattern3 = re.compile(r'^关联方及关联交易$')
 table1_pattern_start = re.compile(r'(重要)?在建工程(项目)?(本期|本年)?变动情况')
 table1_pattern2_start = re.compile(r'\d{1,2}、?\s*在建工程')
 table1_pattern_end = re.compile(
     r'在建工程减值准备情况|在建工程的减值测试情况|工程物资|生产性生物资产|无形资产|递延所得税资产|使用权资产|长期待摊费用')
+table2_pattern_start = re.compile(r'合同资产情况')
+table2_pattern2_start = re.compile(r'\d{1,2}、?\s*合同资产')
+table2_pattern_end = re.compile(
+    r'报告期内账面价值发生重大变动的金额和原因|应收款项融资|应收款项融资分类列示|预付款项|其他应收款')
+table3_pattern_start = re.compile(r'按预付对象归集的期末余额前五名的预付款情况')
+table3_pattern2_start = re.compile(r'\d{1,2}、?\s*预付款项')
+table3_pattern_end = re.compile(
+    r'其他说明|其他应收款|存货')
+
+
+class TableVO:
+    full_text = ""
+    reasoning_content = ""
+    answer_content = ""
+    completion_usage = ""
+    completion_tokens = 0
+    prompt_tokens = 0
+
+    def __init__(self, table_name, table_start_pages, table_start2_pages, table_end_page):
+        self.table_name = table_name
+        self.table_start_pages = table_start_pages
+        self.table_start2_pages = table_start2_pages
+        self.table_end_page = table_end_page
 
 
 def process_pdf(pdf_path):
@@ -43,12 +68,16 @@ def process_pdf(pdf_path):
                     for match in matches:
                         num_str = match.group(1)
                         title = match.group(2).strip()
-                        is_target = bool(target_pattern.match(title))
+                        is_target = bool(target_pattern1.match(title))
+                        is_target2 = bool(target_pattern2.match(title))
+                        is_target3 = bool(target_pattern3.match(title))
                         all_titles.append({
                             'num_str': num_str,
                             'title': title,
                             'page': current_page,
-                            'is_target': is_target
+                            'is_target': is_target,
+                            'is_target2': is_target2,
+                            'is_target3': is_target3
                         })
 
         # 处理序号连续性
@@ -68,65 +97,95 @@ def process_pdf(pdf_path):
                     break
 
         # 获取目标页码
-        target_page = None
-        next_page = None
-        target_indices = [i for i, t in enumerate(all_titles) if t['is_target']]
-        if target_indices:
-            first_target_idx = target_indices[0]
-            target_page = all_titles[first_target_idx]['page']
-            if first_target_idx + 1 < len(all_titles):
-                next_page = all_titles[first_target_idx + 1]['page']
+        target_page, next_page = get_target_page(all_titles, 'is_target')
+        target_page2, next_page2 = get_target_page(all_titles, 'is_target2')
+        target_page3, next_page3 = get_target_page(all_titles, 'is_target3')
 
-        table1_end_page, table1_start2_pages, table1_start_pages = getTable1(pdf, target_page, next_page)
+        table_list = []
 
-        full_text = ""
-        if len(table1_start_pages) < 1:
-            table1_start_pages = table1_start2_pages
-
-        print(f"{table1_start_pages}:{table1_end_page}")
-
-        if table1_end_page is not None and len(table1_start_pages) > 0:
-            for page in pdf.pages[table1_start_pages[0] - 1:table1_end_page]:
-                text = page.extract_text(keep_blank_chars=True, x_tolerance=40)
-                full_text += text
-        print(f"full_text:{full_text}")
-        reasoning_content, answer_content, completion_usage = llmChat.chat_completion(full_text)
-        with lock:
-            counter.value += 1
-            print(f"{counter.value}.{filename}")
-        return {
+        table1_start_pages, table1_start2_pages, table1_end_page = get_table(pdf, target_page, next_page,
+                                                                             table1_pattern_start,
+                                                                             table1_pattern2_start,
+                                                                             table1_pattern_end)
+        table_list.append(TableVO("重要在建工程变动情况", table1_start_pages, table1_start2_pages, table1_end_page))
+        table2_start_pages, table2_start2_pages, table2_end_page = get_table(pdf, target_page, next_page,
+                                                                             table2_pattern_start,
+                                                                             table2_pattern2_start,
+                                                                             table2_pattern_end)
+        table_list.append(TableVO("合同资产情况", table2_start_pages, table2_start2_pages, table2_end_page))
+        table3_start_pages, table3_start2_pages, table3_end_page = get_table(pdf, target_page, next_page,
+                                                                             table3_pattern_start,
+                                                                             table3_pattern2_start,
+                                                                             table3_pattern_end)
+        table_list.append(TableVO("按预付对象归集的期末余额前五名的预付款情况", table3_start_pages, table3_start2_pages,
+                                  table3_end_page))
+        result = {
             '文件名': filename,
             '所有标题': '，'.join([t['title'] for t in all_titles]),
             '是否连续': '是' if is_continuous else '否',
             '财务报表项目注释页码': target_page,
-            '下一个标题的页码': next_page,
-            '重要在建工程变动情况的开始页码': table1_start_pages,
-            '在建工程开始页码': table1_start2_pages,
-            '重要在建工程变动情况的结束页码': table1_end_page,
-            '引用全文': full_text,
-            '思考过程': reasoning_content,
-            '大模型回答': answer_content,
-            '大模型输入tokens': completion_usage.completion_tokens,
-            '大模型输出tokens': completion_usage.prompt_tokens
+            '下一个标题的页码': next_page
         }
+        for index, table_data in enumerate(table_list):
+            full_text = get_full_text(pdf, table_data.table_start_pages, table_data.table_start2_pages,
+                                      table_data.table_end_page)
+            reasoning_content, answer_content, completion_usage = llmChat.chat_completion(full_text, index)
+            table_data.full_text = full_text
+            table_data.reasoning_content = reasoning_content
+            table_data.answer_content = answer_content
+            table_data.completion_tokens = completion_usage.completion_tokens
+            table_data.prompt_tokens = completion_usage.prompt_tokens
+            result[table_data.table_name + '开始页码'] = table_data.table_start_pages
+            result[table_data.table_name + '父亲开始页码'] = table_data.table_start2_pages
+            result[table_data.table_name + '结束页码'] = table_data.table_end_page
+            result[table_data.table_name + '引用全文'] = table_data.full_text
+            result[table_data.table_name + '思考过程'] = table_data.reasoning_content
+            result[table_data.table_name + '大模型回答'] = table_data.answer_content
+            result[table_data.table_name + '大模型输入tokens'] = table_data.completion_tokens
+            result[table_data.table_name + '大模型输出tokens'] = table_data.prompt_tokens
+        with lock:
+            counter.value += 1
+            print(f"{counter.value}.{filename}")
+        return result
 
     except Exception as e:
+        print(f'处理出错：{str(e)}')
         return {
             '文件名': os.path.basename(pdf_path),
             '所有标题': f'处理出错：{str(e)}',
             '是否连续': '否',
             '财务报表项目注释页码': None,
-            '下一个标题的页码': None,
-            '重要在建工程变动情况的页码': None,
-            '在建工程开始页码': None,
-            '重要在建工程变动情况的结束页码': None
+            '下一个标题的页码': None
         }
 
 
-def getTable1(pdf, start_page, end_page):
-    table1_start_pages = []
-    table1_start2_pages = []
-    table1_end_page = None
+def get_target_page(all_titles, target_num):
+    target_page = None
+    next_page = None
+    target_indices = [i for i, t in enumerate(all_titles) if t[target_num]]
+    if target_indices:
+        first_target_idx = target_indices[0]
+        target_page = all_titles[first_target_idx]['page']
+        if first_target_idx + 1 < len(all_titles):
+            next_page = all_titles[first_target_idx + 1]['page']
+    return target_page, next_page
+
+
+def get_full_text(pdf, table_start_pages, table_start2_pages, table_end_page):
+    full_text = ""
+    if len(table_start_pages) < 1:
+        table_start_pages = table_start2_pages
+    if table_end_page is not None and len(table_start_pages) > 0 and table_start_pages[0] - table_end_page < 20:
+        for page in pdf.pages[table_start_pages[0] - 1:table_end_page]:
+            text = page.extract_text(keep_blank_chars=True, x_tolerance=40)
+            full_text += text
+    return full_text
+
+
+def get_table(pdf, start_page, end_page, table_pattern_start, table_pattern2_start, table_pattern_end):
+    table_start_pages = []
+    table_start2_pages = []
+    table_end_page = None
     if end_page:
         for page_num, page in enumerate(pdf.pages[start_page - 1:end_page - 1]):
             words = page.extract_words(keep_blank_chars=True, x_tolerance=40)
@@ -134,28 +193,28 @@ def getTable1(pdf, start_page, end_page):
                 # 提取文案
                 word_text = word.get('text').replace(" ", "")
                 text_clean = re.sub(r'\s+', ' ', word_text).strip()
-                matches = table1_pattern_start.findall(text_clean)
+                matches = table_pattern_start.findall(text_clean)
                 for match in matches:
-                    table1_start_pages.append(start_page + page_num)
-                if (len(table1_start_pages) > 0
-                        and table1_end_page is None
-                        and table1_pattern_end.search(text_clean) is not None):
-                    table1_end_page = start_page + page_num
-    if len(table1_start_pages) < 1 and len(table1_start2_pages) < 1:
+                    table_start_pages.append(start_page + page_num)
+                if (len(table_start_pages) > 0
+                        and table_end_page is None
+                        and table_pattern_end.search(text_clean) is not None):
+                    table_end_page = start_page + page_num
+    if len(table_start_pages) < 1 and len(table_start2_pages) < 1:
         for page_num, page in enumerate(pdf.pages[start_page - 1:end_page - 1]):
             words = page.extract_words(keep_blank_chars=True, x_tolerance=40)
             for word in words:
                 # 提取文案
                 word_text = word.get('text').replace(" ", "")
                 text_clean = re.sub(r'\s+', ' ', word_text).strip()
-                matches = table1_pattern2_start.findall(text_clean)
+                matches = table_pattern2_start.findall(text_clean)
                 for match in matches:
-                    table1_start2_pages.append(start_page + page_num)
-                if (len(table1_start2_pages) > 0
-                        and table1_end_page is None
-                        and table1_pattern_end.search(text_clean) is not None):
-                    table1_end_page = start_page + page_num
-    return table1_end_page, table1_start2_pages, table1_start_pages
+                    table_start2_pages.append(start_page + page_num)
+                if (len(table_start2_pages) > 0
+                        and table_end_page is None
+                        and table_pattern_end.search(text_clean) is not None):
+                    table_end_page = start_page + page_num
+    return table_start_pages, table_start2_pages, table_end_page
 
 
 # 全局变量（子进程中共享）
@@ -177,10 +236,10 @@ def main(folder_path, output_file):
         for f in os.listdir(folder_path)
         if f.lower().endswith('.pdf')
     ]
-    # pdf_files = list(
-    #     filter(lambda s:
-    #            "安徽安凯汽车股份有限公司.pdf" in s or
-    #            "111.pdf" in s, pdf_files))
+    pdf_files = list(
+        filter(lambda s:
+               "安徽安凯汽车股份有限公司.pdf" in s or
+               "111.pdf" in s, pdf_files))
 
     counter = Value("i", 0)  # 主进程创建共享变量
     lock = Lock()  # 主进程创建锁
